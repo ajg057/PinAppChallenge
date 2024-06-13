abd440299ea4c24a5b9982e0ec0eeb5a03f302b0
ajg057 <jmgonzalez@gmail.com>
2024-06-13 18:10:17 -0300

    Revert "[Feature]: average age (#3)"
    This reverts commit 212375c3756add14288827ac8e25321ad0c23ef2.

--- pinapp/src/main/java/com/challenge/pinapp/service/ClientService.java
+++
@@ -1,9 +0,0 @@
-package com.challenge.pinapp.service;
-
-import com.challenge.pinapp.dto.ClientDTO;
-import com.challenge.pinapp.model.Client;
-
-public interface ClientService {
-    Client createClient(ClientDTO clientDTO);
-    double getAverageAge();
-}
