90e51b99893062682a6f11b23ca21015c64f1f26
ajg057 <jmgonzalez@gmail.com>
2024-06-13 13:43:24 -0300

    feature(): add initial scaffolding (#1)

---
+++ pinapp/src/test/java/com/challenge/pinapp/PinappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.pinapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
