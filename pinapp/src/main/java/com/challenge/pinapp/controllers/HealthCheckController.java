4cddad96de7e841941687f90646939a891e659a7
JuanMa <jmgonzalez@gmail.com>
2024-06-13 13:32:56 -0300

    feature(): add initial scaffolding

---
+++ pinapp/src/main/java/com/challenge/pinapp/controllers/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.challenge.pinapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Application is running!";
+    }
+}
